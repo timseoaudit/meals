ca3dbdd9e8ab7eeb40b5a204268982bc1f8a8451
Timneji <139955017+timseoaudit@users.noreply.github.com>
2024-05-14 01:18:12 +0100

    add Category Meal view
    Shows now meals for each category with pictures, name, times complexity and price/

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,17 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectedCategory(BuildContext context) {
+  void _selectedCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (ctx) => const MealsScreen(
-          title: "Some Title",
-          meals: [],
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -40,7 +45,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectedCategory(context);
+                _selectedCategory(context, category);
               },
             ),
         ],
