bcf00397fcede6ab4fefef924833eebecca018f8
Timneji <139955017+timseoaudit@users.noreply.github.com>
2024-05-15 23:10:37 +0100

    Add Tabbar
    Added Tabbar for Navigation between categories and favorites

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -4,9 +4,9 @@ import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key, required this.title, required this.meals});
+  const MealsScreen({super.key, this.title, required this.meals});
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(
@@ -53,10 +53,15 @@ class MealsScreen extends StatelessWidget {
         ]),
       );
     }
+
+    if (title == null) {
+      return content; 
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            title,
+            title!,
           ),
         ),
         body: content);
